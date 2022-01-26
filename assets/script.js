f7f8e6b6fc5794fef60fb0db3272945a2e0ff3be
Daysu Quispe <dayday@Daysus-MBP.fios-router.home>
2022-01-26 15:39:28 -0500

    demo

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,131 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
+console.log('JavaScript code has loaded!')
+
+let yourName = "Daysu Quispe" 
+
+
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let cookieAverage = 0 // Cookie average
+let totalCookie = document.querySelector('#qty-total')
 
-// selecting the element with an id of credit
+
+
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
-
-// Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+
+// GB
+const gbPlusBtn = document.querySelector('#add-gb')
+const gbTotal = document.querySelector('#qty-gb')
+
+
+
+
+// GB add
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
+    gb = gb + 1;
+    gbTotal.textContent = `${gb}`;
+
+    cookieAverage = gb + cc + sugar;
+    totalCookie.textContent = cookieAverage;
+
 console.log('Gingerbread + button was clicked!')
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+});
+
+// GB min
+let gbMinBtn = document.querySelector('#minus-gb');
+gbMinBtn.addEventListener('click', function() {
+    if (gb > 0) {
+        gb = gb - 1;
+    }
+    gbTotal.textContent = `${gb}`;
+
+    cookieAverage = gb + cc + sugar;
+    totalCookie.textContent = cookieAverage;
+
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+
+
+
+
+
+// CC
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccTotal = document.querySelector('#qty-cc')
+
+
+
+
+// CC add
+ccPlusBtn.addEventListener('click', function() {
+    cc = cc + 1;
+    ccTotal.textContent = `${cc}`;
+
+    cookieAverage = gb + cc + sugar;
+    totalCookie.textContent = cookieAverage;
+
+
+
+console.log('Chocolate Chip + button was clicked!')
+
+});
+
+// CC min
+let ccMinBtn = document.querySelector('#minus-cc');
+ccMinBtn.addEventListener('click', function() {
+    if (cc > 0) {
+        cc = cc - 1;
+    }
+    ccTotal.textContent = `${cc}`;
+
+    cookieAverage = gb + cc + sugar;
+    totalCookie.textContent = cookieAverage;
+
+})
+
+
+
+
+
+
+
+
+// Sugar
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarTotal = document.querySelector('#qty-sugar')
+
+
+
+
+// Sugar add
+sugarPlusBtn.addEventListener('click', function() {
+    sugar = sugar + 1;
+    sugarTotal.textContent = `${sugar}`;
+
+    cookieAverage = gb + cc + sugar;
+    totalCookie.textContent = cookieAverage;
+
+
+
+console.log('Sugar + button was clicked!')
+
+});
+
+// Sugar min
+let sugarMinBtn = document.querySelector('#minus-sugar');
+sugarMinBtn.addEventListener('click', function() {
+    if (sugar > 0) {
+        sugar = sugar - 1;
+    }
+    sugarTotal.textContent = `${sugar}`;
+
+    cookieAverage = gb + cc + sugar;
+    totalCookie.textContent = cookieAverage;
+
+})
+
